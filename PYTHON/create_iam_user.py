5c72813e51edf77700e7084f61e9b01e745b9920
AVI8-R G <azal88@yahoo.com>
2020-11-28 14:42:20 -0600

    a

--- PYTHON/create_iam_user.py
+++ PYTHON/create_iam_user.py
@@ -7,21 +7,22 @@ def get_iam_client():
 def get_random_passwd():
     len_of_passwd=10
     valid_chars_for_passwd='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~'
-    try:
-        passwd=[choice(valid_chars_for_passwd) for i in range(len_of_passwd)]
-        return "".join(passwd)
-    except:
-        pass
+    passwd=[choice(valid_chars_for_passwd) for i in range(len_of_passwd)]
+    return "".join(passwd)
+    
 
 
 
 def main():
     iam_client=get_iam_client()
-    iam_username=#enter user name in string
+    iam_username=#enter user name as string
     passwd=get_random_passwd()
     PolicyArn='arn:aws:iam::aws:policy/AdministratorAccess'
     iam_client.create_user(UserName=iam_username)
-    iam_client.create_login_profile(UserName=iam_username, Password=passwd, PasswordResetRequired=True)
+    try:
+        iam_client.create_login_profile(UserName=iam_username, Password=passwd, PasswordResetRequired=True)
+    except:
+        pass
     iam_client.attach_user_policy(UserName=iam_username, PolicyArn=PolicyArn)
     print('Iam username={} and password={}'.format(iam_username,passwd))
     return 'Iam username={} and password={}'.format(iam_username,passwd)
