b558fbc9d150ab7d0ba4845fd53b8b5ade851cbf
AVI8-R G <azal88@yahoo.com>
2021-10-20 17:28:50 -0500

    added new code

--- PYTHON/test/test.py
+++ PYTHON/test/test.py
@@ -1,114 +1,115 @@
-import re
-import pandas as pd
+
 def converter():
-    availibility_list = []
-    realmed_list = []
-    hews_list = []
-
-    with open('C:/Users/avi8r/Desktop/python/PYTHON/test/test.txt','r') as test_file:   
-        line = [line for line in test_file] 
-        count =0
-        for i in line:
-            if '030240928' in i:
-                availibility_list.append(i)
-            if '030240122'  in i:
-                count+=1                
-                realmed_list.append(i)
-            if '030550127' in i:
-                hews_list.append(i)
-        #             realmed_list.append(new_line)
-    #     for line in test_file:
-    #         new_line =re.split('\~|\*', line)
-    #         print(new_line)
-    #         if '030240928' in new_line:
-    #             availibility_list.append(new_line)
-    #             print(new_line)
-    #         elif '030240122' in new_line:
-    #             realmed_list.append(new_line)
-                
-    #         elif '030550127' in new_line:
-    #             hews_list.append(new_line)
-    # print(count)
-    # print(realmed_list)
-    for i in realmed_list:
-        if '621' in i:
-            il=i
-            
-            if '1030223' in il:
-                _270 = il
-                print(il)
-            if '191703' in il:
-                _271=il    
-                print(il)                
-            if '085707' in il:
-                _276 = il
-                print(il)
-            if '0877077' in il:
-                _277=il
-                print(il)
-                    
-        if '751' in i:
-            mt=i
-            
-            if '1030223' in mt:
-                _270 = mt
-                print(mt)
-            if '191703' in mt:
-                _271=mt    
-                print(mt)                
-            if '085707' in mt:
-                _276 = mt
-                print(mt)
-            if '0877077' in mt:
-                _277=mt
-                print(mt)
-        if '290' in i:
-            nm =i
-            
-            if '1030223' in nm:
-                _270 = nm
-                print(nm)
-            if '191703' in nm:
-                _271=nm   
-                print(nm)                 
-            if '085707' in nm:
-                _276 = nm
-                print(nm)
-            if '0877077' in nm:
-                _277=nm
-                print(nm)
-        if '340' in i:
-            ok = i
-            
-            if '1030223' in ok:
-                _270 = ok
-                print(ok)
-            if '191703' in ok:
-                _271=ok  
-                print(ok)                  
-            if '085707' in ok:
-                _276 = ok
-                print(ok)
-            if '0877077' in ok:
-                _277=ok
-                print(ok)
-        if '400' in i:
-            tx=i
-            
-            if '1030223' in tx:
-                _270 = tx
-                print(tx)
-            if '191703' in tx:
-                _271=tx    
-                print(tx)               
-            if '085707' in tx:
-                _276 = tx
-                print(tx)
-            if '0877077' in tx:
-                _277=tx
-                print(tx)
-
-
-
+    
+    print('MONTHLY   LOCAL')
+    print('Vendor    TYPE    IL        MT       NM       OK        TX')
+    with open('./test.txt','r') as test_file: 
+        avility=[line for line in test_file if '*030240928' in line]
+        test_file.close()
+    with open('./test.txt','r') as test_file:     
+        realmed=[line for line in test_file if '*030240122' in line]
+        test_file.close()
+    with open('./test.txt','r') as test_file: 
+        hews=[line for line in test_file if '*030550127' in line]
+        test_file.close()
+        
+    ############################ Availity ###############################
+    type_270 = [i.split('        ')[3] for i in avility if '*1030223*' in i]   
+    
+    il_270 = [i for i in type_270 if "*621*" in i]
+    mt_270 = [i for i in type_270 if "*751*" in i]
+    nm_270 = [i for i in type_270 if "*290*" in i]
+    ok_270 = [i for i in type_270 if "*340*" in i]
+    tx_270 = [i for i in type_270 if "*400*" in i]
+    #####################################
+    type_271 = [i.split('        ')[3] for i in avility if '*191703*' in i]
+    il_271 = [i for i in type_271 if "*621*" in i]
+    mt_271 = [i for i in type_271 if "*751*" in i]
+    nm_271 = [i for i in type_271 if "*290*" in i]
+    ok_271 = [i for i in type_271 if "*340*" in i]
+    tx_271 = [i for i in type_271 if "*400*" in i]
+    ######################################
+    type_276 = [i.split('        ')[3] for i in avility if '*085707*' in i]
+    il_276 = [i for i in type_276 if "*621*" in i]
+    mt_276 = [i for i in type_276 if "*751*" in i]
+    nm_276 = [i for i in type_276 if "*290*" in i]
+    ok_276 = [i for i in type_276 if "*340*" in i]
+    tx_276 = [i for i in type_276 if "*400*" in i]
+    #####################################
+    type_277 = [i.split('        ')[3] for i in avility if '*0877077*' in i]
+    il_277 = [i for i in type_277 if "*621*" in i]
+    mt_277 = [i for i in type_277 if "*751*" in i]
+    nm_277 = [i for i in type_277 if "*290*" in i]
+    ok_277 = [i for i in type_277 if "*340*" in i]
+    tx_277 = [i for i in type_277 if "*400*" in i]
+    #################********************#####################
+    print('Availity','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
+    print('Availity','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
+    print('Availity','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
+    print('Availity','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
+    ######################################### RealMed ##################################################
+    type_270 = [i.split('        ')[3] for i in realmed if '*1030223*' in i]    
+    il_270 = [i for i in type_270 if "*621*" in i]
+    mt_270 = [i for i in type_270 if "*751*" in i]
+    nm_270 = [i for i in type_270 if "*290*" in i]
+    ok_270 = [i for i in type_270 if "*340*" in i]
+    tx_270 = [i for i in type_270 if "*400*" in i]
+    #####################################
+    type_271 = [i.split('        ')[3] for i in realmed if '*191703*' in i]
+    il_271 = [i for i in type_271 if "*621*" in i]
+    mt_271 = [i for i in type_271 if "*751*" in i]
+    nm_271 = [i for i in type_271 if "*290*" in i]
+    ok_271 = [i for i in type_271 if "*340*" in i]
+    tx_271 = [i for i in type_271 if "*400*" in i]
+    ######################################
+    type_276 = [i.split('        ')[3] for i in realmed if '*085707*' in i]
+    il_276 = [i for i in type_276 if "*621*" in i]
+    mt_276 = [i for i in type_276 if "*751*" in i]
+    nm_276 = [i for i in type_276 if "*290*" in i]
+    ok_276 = [i for i in type_276 if "*340*" in i]
+    tx_276 = [i for i in type_276 if "*400*" in i]
+    #####################################
+    type_277 = [i.split('        ')[3] for i in realmed if '*0877077*' in i]
+    il_277 = [i for i in type_277 if "*621*" in i]
+    mt_277 = [i for i in type_277 if "*751*" in i]
+    nm_277 = [i for i in type_277 if "*290*" in i]
+    ok_277 = [i for i in type_277 if "*340*" in i]
+    tx_277 = [i for i in type_277 if "*400*" in i]
+    print('RealMed ','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
+    print('RealMed ','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
+    print('RealMed ','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
+    print('RealMed ','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
+    ########################################## Hews ##################################
+    type_270 = [i.split('        ')[3] for i in hews if '*1030223*' in i]    
+    il_270 = [i for i in type_270 if "*621*" in i]
+    mt_270 = [i for i in type_270 if "*751*" in i]
+    nm_270 = [i for i in type_270 if "*290*" in i]
+    ok_270 = [i for i in type_270 if "*340*" in i]
+    tx_270 = [i for i in type_270 if "*400*" in i]
+    #####################################
+    type_271 = [i.split('        ')[3] for i in hews if '*191703*' in i]
+    il_271 = [i for i in type_271 if "*621*" in i]
+    mt_271 = [i for i in type_271 if "*751*" in i]
+    nm_271 = [i for i in type_271 if "*290*" in i]
+    ok_271 = [i for i in type_271 if "*340*" in i]
+    tx_271 = [i for i in type_271 if "*400*" in i]
+    ######################################
+    type_276 = [i.split('        ')[3] for i in hews if '*085707*' in i]
+    il_276 = [i for i in type_276 if "*621*" in i]
+    mt_276 = [i for i in type_276 if "*751*" in i]
+    nm_276 = [i for i in type_276 if "*290*" in i]
+    ok_276 = [i for i in type_276 if "*340*" in i]
+    tx_276 = [i for i in type_276 if "*400*" in i]
+    #####################################
+    type_277 = [i.split('        ')[3] for i in hews if '*0877077*' in i]
+    il_277 = [i for i in type_277 if "*621*" in i]
+    mt_277 = [i for i in type_277 if "*751*" in i]
+    nm_277 = [i for i in type_277 if "*290*" in i]
+    ok_277 = [i for i in type_277 if "*340*" in i]
+    tx_277 = [i for i in type_277 if "*400*" in i]
+    print('Hews  ','  270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
+    print('Hews  ','  271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
+    print('Hews  ','  276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
+    print('Hews  ','  277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
 
 converter()
