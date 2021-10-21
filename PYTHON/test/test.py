4285a6da378d5357b3be975d2c75679b6a3c2c03
AVI8-R G <azal88@yahoo.com>
2021-10-21 17:17:58 -0500

    updated

--- PYTHON/test/test.py
+++ PYTHON/test/test.py
@@ -1,15 +1,15 @@
-
+import os,os.path,sys
 def converter():
-    
-    print('MONTHLY   LOCAL')
-    print('Vendor    TYPE    IL        MT       NM       OK        TX')
-    with open('./test.txt','r') as test_file: 
+    fileName = os.path.join(workingDirectory,'../test.txt')
+    print('MONTHLY  LOCAL')
+    print('Vendor       TYPE      IL     MT     NM     OK     TX')
+    with open(fileName,'r') as test_file: 
         avility=[line for line in test_file if '*030240928' in line]
         test_file.close()
-    with open('./test.txt','r') as test_file:     
+    with open(fileName,'r') as test_file:     
         realmed=[line for line in test_file if '*030240122' in line]
         test_file.close()
-    with open('./test.txt','r') as test_file: 
+    with open(fileName,'r') as test_file: 
         hews=[line for line in test_file if '*030550127' in line]
         test_file.close()
         
@@ -42,11 +42,7 @@ def converter():
     nm_277 = [i for i in type_277 if "*290*" in i]
     ok_277 = [i for i in type_277 if "*340*" in i]
     tx_277 = [i for i in type_277 if "*400*" in i]
-    #################********************#####################
-    print('Availity','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
-    print('Availity','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
-    print('Availity','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
-    print('Availity','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
+    print('Availity','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270),'\nAvaility','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271),'\nAvaility','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276),'\nAvaility','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
     ######################################### RealMed ##################################################
     type_270 = [i.split('        ')[3] for i in realmed if '*1030223*' in i]    
     il_270 = [i for i in type_270 if "*621*" in i]
@@ -75,10 +71,7 @@ def converter():
     nm_277 = [i for i in type_277 if "*290*" in i]
     ok_277 = [i for i in type_277 if "*340*" in i]
     tx_277 = [i for i in type_277 if "*400*" in i]
-    print('RealMed ','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
-    print('RealMed ','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
-    print('RealMed ','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
-    print('RealMed ','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
+    print('RealMed ','270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270),'\nRealMed ','271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271),'\nRealMed ','276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276),'\nRealMed ','277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
     ########################################## Hews ##################################
     type_270 = [i.split('        ')[3] for i in hews if '*1030223*' in i]    
     il_270 = [i for i in type_270 if "*621*" in i]
@@ -107,9 +100,8 @@ def converter():
     nm_277 = [i for i in type_277 if "*290*" in i]
     ok_277 = [i for i in type_277 if "*340*" in i]
     tx_277 = [i for i in type_277 if "*400*" in i]
-    print('Hews  ','  270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270), sep= '      ')
-    print('Hews  ','  271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271), sep= '      ')
-    print('Hews  ','  276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276), sep= '      ')
-    print('Hews  ','  277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
-
-converter()
+    print('Hews  ','  270',len(il_270),len(mt_270),len(nm_270),len(ok_270),len(tx_270),'\nHews  ','  271',len(il_271),len(mt_271),len(nm_271),len(ok_271),len(tx_271),'\nHews  ','  276',len(il_276),len(mt_276),len(nm_276),len(ok_276),len(tx_276),'\nHews  ','  277',len(il_277),len(mt_277),len(nm_277),len(ok_277),len(tx_277), sep= '      ')
+    
+if "__main__" == __name__:
+    workingDirectory  = os.path.realpath(sys.argv[0])
+    converter()
