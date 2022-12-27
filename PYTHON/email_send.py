9a7526767ae3a03f10192b3f0cd8c8cf5ef584aa
Akmanai Turganbaeva <azal88@yahoo.com>
2022-12-26 22:35:06 -0600

    logger

--- PYTHON/email_send.py
+++ PYTHON/email_send.py
@@ -1,15 +1,35 @@
 import smtplib
-import getpass
+import getpass,os,logging
+
+logging.basicConfig(filename="app_log.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='a')
+
+# Creating an object
+logger = logging.getLogger()
+ 
+# Setting the threshold of logger to DEBUG
+logger.setLevel(logging.INFO)
+ 
+# Test messages
+# logger.debug("Harmless debug Message")
+
+# logger.warning("Its a Warning")
+# logger.error("Did you try to divide by zero")
+# logger.critical("Internet is down")
+
+
 smtp_object= smtplib.SMTP('smtp.gmail.com',587)
 smtp_object.ehlo()
 smtp_object.starttls()
 #passwd=input('whats your passwd?: ')
-email = getpass.getpass('Email: ')
-passwd = getpass.getpass('password please: ') #nujen app password
-smtp_object.login(email, passwd)
-from_address=email
-to_address=input('enter reciever email address: ')
-subject = input('enter your subject line: ')
-message = input('enter your message body: ')
+sender_address = os.environ.get('EMAIL', '')
+sender_pass = os.environ.get('PASSWORD', '')
+smtp_object.login(sender_address, sender_pass)
+from_address=sender_address
+to_address= 'azal88@gmail.com'#input('enter reciever email address: ')
+subject = 'Test Subject'#input('enter your subject line: ')
+message = 'Test Message!'#input('enter your message body: ')
 msg = 'subject: '+ subject+'\n' + message
 smtp_object.sendmail(from_address, to_address,msg)
+logger.info(f'{sender_address} send to {to_address} with {subject}.')
